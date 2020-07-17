54569fb22dc74c5dd9f7e30ec6a62bc2518d4ca3
Lint Action <lint-action@samuelmeuli.com>
2020-07-17 17:05:10 +0000

    Fix code style issues with Black

--- youtyper/assets/famous_texts.py
+++ youtyper/assets/famous_texts.py
@@ -5,6 +5,4 @@ universal_declaration_of_human_rights_text = read_text(
 )
 
 # https://ja.wikipedia.org/wiki/%E3%83%91%E3%83%B3%E3%82%B0%E3%83%A9%E3%83%A0
-pangrams_text = read_text(
-    "youtyper.assets", "pangrams.txt"
-)
+pangrams_text = read_text("youtyper.assets", "pangrams.txt")
