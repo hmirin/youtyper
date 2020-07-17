568ef0567ed4e1562988f69c3bf4409b4e791da9
hmirin <panda.1192@gmail.com>
2020-07-18 02:04:18 +0900

    fix bugs

--- youtyper/assets/famous_texts.py
+++ youtyper/assets/famous_texts.py
@@ -6,5 +6,5 @@ universal_declaration_of_human_rights_text = read_text(
 
 # https://ja.wikipedia.org/wiki/%E3%83%91%E3%83%B3%E3%82%B0%E3%83%A9%E3%83%A0
 pangrams_text = read_text(
-    "youtyper.assets", "universal_declaration_of_human_rights.txt"
+    "youtyper.assets", "pangrams.txt"
 )
